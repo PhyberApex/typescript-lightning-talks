11b3554d5a1b59b6aa8b1a41a4c072cd1f899b1f
PhyberApex <PhyberApex@users.noreply.github.com>
2024-10-01 21:12:53 +0000

    deploy: 001cf6ab10a1e9cfdd44a9955ce84d1e00c3550d

---
+++ 03-interface-vs-type-alias/assets/slidev/default-BKC6Vj7G.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-B-QgNJYB.js";import{_ as r}from"../index-H5Q2DLJb.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
