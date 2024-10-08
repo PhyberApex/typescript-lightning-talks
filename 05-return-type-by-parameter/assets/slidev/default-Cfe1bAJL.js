4575f3bee263033b23c92b16facf31d5a71f4c21
PhyberApex <PhyberApex@users.noreply.github.com>
2024-10-08 07:15:04 +0000

    deploy: 131385d7d4c8fa73120dab1ab50801218d4b9ced

---
+++ 05-return-type-by-parameter/assets/slidev/default-Cfe1bAJL.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-CMEZQT3C.js";import{_ as r}from"../index-BT4u1U49.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
