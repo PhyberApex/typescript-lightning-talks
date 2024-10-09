1adaefc2c17c7a504967475e72c422d556b764d3
PhyberApex <PhyberApex@users.noreply.github.com>
2024-10-09 07:06:54 +0000

    deploy: d5364292614721de59e4fc97c189194bbfab5048

--- 03-interface-vs-type-alias/assets/slidev/default-FlRLG4sg.js
+++
@@ -1,1 +0,0 @@
-import{o,b as t,f as s}from"../modules/vue-CjsfXi19.js";import{_ as r}from"../index-Brz3bpWc.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
