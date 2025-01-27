a3b5e94026ebcb3cf7586c5ebbe78f2e59917ed6
PhyberApex <PhyberApex@users.noreply.github.com>
2025-01-27 09:25:51 +0000

    deploy: 72697acc9e8f146ef4f325eb0edef549a0bbb837

---
+++ 08-dont-use-shorthand-syntax/assets/slidev/default-LCNCG_GV.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-Bj6g3SET.js";import{_ as r}from"../index-gb5Hn3On.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
