fa732e166a33ecd5370b0b0b9b3d71c2f4cedd32
PhyberApex <PhyberApex@users.noreply.github.com>
2025-02-26 10:05:52 +0000

    deploy: 52316c8ba3c488a68bba8d7b1de4815fd7d78996

---
+++ 07-indexed-types/assets/slidev/center-TCYtmYZ4.js
@@ -0,0 +1,1 @@
+import{f as t,o,g as s,j as c}from"../modules/vue-X0MnRlWk.js";import{_ as r}from"../index-D11e6kYH.js";const a={},n={class:"slidev-layout center h-full grid place-content-center"},l={class:"my-auto"};function d(e,_){return o(),t("div",n,[s("div",l,[c(e.$slots,"default")])])}const m=r(a,[["render",d]]);export{m as I};
