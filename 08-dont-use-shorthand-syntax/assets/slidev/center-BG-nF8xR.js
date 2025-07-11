28e7d0ecf2905e1085141d3c468da180b2056e3e
PhyberApex <PhyberApex@users.noreply.github.com>
2025-07-11 15:26:21 +0000

    deploy: 8383931536270a619472d9a0fb586f7ca4bf8b37

---
+++ 08-dont-use-shorthand-syntax/assets/slidev/center-BG-nF8xR.js
@@ -0,0 +1,1 @@
+import{f as t,o,g as s,j as c}from"../modules/vue-CYsd2D4n.js";import{_ as r}from"../index-DLyK3hvJ.js";const a={},n={class:"slidev-layout center h-full grid place-content-center"},l={class:"my-auto"};function d(e,_){return o(),t("div",n,[s("div",l,[c(e.$slots,"default")])])}const m=r(a,[["render",d]]);export{m as I};
