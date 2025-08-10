232ad3f987b1a21af26d6e5a41ec31913aaa1534
PhyberApex <PhyberApex@users.noreply.github.com>
2025-08-10 19:52:14 +0000

    deploy: 3e336adfd76ea406631e8e641ea9837b03fdb46f

--- 03-interface-vs-type-alias/assets/slidev/default-l8IRSdrT.js
+++
@@ -1,1 +0,0 @@
-import{f as o,o as t,j as s}from"../modules/vue-DrMFpiPl.js";import{_ as r}from"../index-Dwn92Udq.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
