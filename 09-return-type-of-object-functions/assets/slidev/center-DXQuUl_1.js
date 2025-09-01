37684b182797899cfc105db0472755ac3aef2208
PhyberApex <PhyberApex@users.noreply.github.com>
2025-09-01 09:36:49 +0000

    deploy: f84491f0b87bacd8c3bf7bcd47d9e459f23d28bd

--- 09-return-type-of-object-functions/assets/slidev/center-DXQuUl_1.js
+++
@@ -1,1 +0,0 @@
-import{f as t,o,g as s,j as c}from"../modules/vue-CbmGV-0n.js";import{_ as r}from"../index-CY4jFXI5.js";const a={},n={class:"slidev-layout center h-full grid place-content-center"},l={class:"my-auto"};function d(e,_){return o(),t("div",n,[s("div",l,[c(e.$slots,"default")])])}const m=r(a,[["render",d]]);export{m as I};
