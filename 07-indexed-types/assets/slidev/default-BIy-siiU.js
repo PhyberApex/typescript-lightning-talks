a254262ab21fe33a277a14a950a4a4fe783ff006
PhyberApex <PhyberApex@users.noreply.github.com>
2025-11-06 11:01:51 +0000

    deploy: c57540a44ae262e8914fc54726bdb8d65922fbeb

---
+++ 07-indexed-types/assets/slidev/default-BIy-siiU.js
@@ -0,0 +1,1 @@
+import{f as o,o as t,j as s}from"../modules/vue-mMgBWq5X.js";import{_ as r}from"../index-DMcb3lF0.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
