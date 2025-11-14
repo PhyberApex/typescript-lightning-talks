62148c21c142e27d5f7553bd90cbc6fb515a805d
PhyberApex <PhyberApex@users.noreply.github.com>
2025-11-14 13:29:54 +0000

    deploy: 0f50e4d8f63a65c6a3bf0146851cd2ff1ba47464

---
+++ 04-dont-use-enums/assets/slidev/center-BgvYwocR.js
@@ -0,0 +1,1 @@
+import{f as t,o,g as s,j as c}from"../modules/vue-6H69zlv8.js";import{_ as r}from"../index-BvFhW_7X.js";const a={},n={class:"slidev-layout center h-full grid place-content-center"},l={class:"my-auto"};function d(e,_){return o(),t("div",n,[s("div",l,[c(e.$slots,"default")])])}const m=r(a,[["render",d]]);export{m as I};
